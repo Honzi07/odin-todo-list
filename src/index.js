475dad22d674e2e45540f9e43672d968009ba24e
Honzi07 <nyisztor.zsolt@gmail.com>
2023-09-29 03:54:49 +0200

    Initial commit.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './scss/style.scss';
+
+console.log('test');
+
+const sayHello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+sayHello('John');
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
