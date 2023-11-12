62353929af57ef86a0442e133f66098c9a7a1c9b
Honzi07 <nyisztor.zsolt@gmail.com>
2023-11-12 03:02:30 +0100

    Some function for testing the modal.

--- src/index.js
+++ src/index.js
@@ -4,15 +4,36 @@ import './scss/reset.scss';
 const menuBtn = document.querySelector('.btn-menu');
 const asideEL = document.querySelector('aside');
 const mainEL = document.querySelector('main');
-const projectEl = document.querySelector('.project');
+const createProjectBtn = document.querySelector('.btn-create-project');
+const modal = document.querySelector('.modal');
+const modalEl = document.querySelector('.modal-content');
+const modalCloseBtn = document.querySelector('.modal-close');
+const projectEl = document.querySelectorAll('.project');
 const projectInputContainer = document.querySelector(
   '.project-input-container'
 );
+
+// console.log(projectEl, projectInputContainer);
+console.log(modalCloseBtn);
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
   asideEL.classList.toggle('active');
   mainEL.classList.toggle('active');
+});
+
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+modalCloseBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+createProjectBtn.addEventListener('click', () => {
+  modal.style.display = 'flex';
 });
 
 function mouseOver() {
@@ -28,9 +49,35 @@ function mouseLeft() {
   }
 }
 
-projectEl.addEventListener('click', () => {
-  projectInputContainer.classList.add('active');
-});
+// projectEl.addEventListener('click', () => {
+//   projectInputContainer.classList.add('active');
+// });
 
-projectEl.addEventListener('mouseenter', mouseOver);
-projectEl.addEventListener('mouseleave', mouseLeft);
+// projectEl.addEventListener('mouseenter', mouseOver);
+// projectEl.addEventListener('mouseleave', mouseLeft);
+
+const checkbox = document.querySelector('.todo-checkbox');
+checkbox.addEventListener('click', logCheckbox);
+
+function logCheckbox() {
+  // if (checkbox.checked) {
+  //   console.log('pipa');
+  // } else console.log('nincs pipa');
+
+  checkbox.checked ? console.log('pipa') : console.log('nincs pipa');
+}
+
+// const input = document.querySelectorAll('.modal input[type=text]');
+// const label = document.querySelector('form div label');
+
+// console.log(input, input.nextElementSibling);
+
+// function floatLabel() {
+//   if (input.value.length > 0) {
+//     input.nextElementSibling.classList.add('float-label');
+//   } else {
+//     input.nextElementSibling.classList.remove('float-label');
+//   }
+// }
+
+// input.forEach((el) => el.addEventListener('input', floatLabel));
