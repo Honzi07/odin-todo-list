e70e0b54f9d005c5e8283452b224a17413f53e07
Honzi07 <nyisztor.zsolt@gmail.com>
2024-04-28 05:50:53 +0200

    Dom class for different DOM affecting methods and method for return modal input values.

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,117 +1,15 @@
-const openModalBtn = document.querySelector('.btn-open-modal');
-const modal = document.querySelector('.modal');
-const modalEl = document.querySelector('.modal-content');
-const modalCloseBtn = document.querySelector('.modal-close');
-const radioFieldset = document.querySelector('.input-radio');
-const inputProject = document.querySelector('#project');
-const inputTodo = document.querySelector('#todo');
-const inputTitle = document.querySelector('#title');
-const inputContent = document.querySelector('#content');
-const inputDate = document.querySelector('#date');
-const modalSubmitBtn = document.querySelector('button[type=submit]');
-const modalForm = document.querySelector('.modal-content form');
+export default class DOM {
+  constructor() {}
 
-const btnAddProjectTodo = document.querySelector('.btn-add-project-todo');
+  getModalInputValues() {
+    const inputTitle = document.querySelector('#title');
+    const inputContent = document.querySelector('#content');
+    const inputDate = document.querySelector('#date');
 
-function changeInputsState() {
-  if (inputTodo.checked) {
-    selectTodo();
-  } else {
-    selectProject();
+    return {
+      title: inputTitle.value,
+      description: inputContent.value,
+      date: inputDate.valueAsNumber,
+    };
   }
 }
-
-function selectTodo() {
-  inputTitle.removeAttribute('disabled');
-  inputTitle.setAttribute('disabled', '');
-  inputTitle.classList.add('disabled');
-  inputTitle.labels[0].classList.add('disabled');
-  inputContent.setAttribute('required', '');
-}
-
-function selectProject() {
-  inputTitle.removeAttribute('disabled');
-  inputTitle.setAttribute('required', '');
-  inputTitle.classList.remove('disabled');
-  inputTitle.labels[0].classList.remove('disabled');
-  inputContent.removeAttribute('required');
-  inputContent.setAttribute('required', '');
-}
-
-[inputTodo, inputProject].forEach((el) => {
-  el.addEventListener('click', changeInputsState);
-});
-
-function showEditModal() {
-  modalEl.dataset.mode = 'edit';
-  modal.style.display = 'flex';
-  radioFieldset.style.pointerEvents = 'none';
-  inputTodo.checked = true;
-  inputTitle.setAttribute('disabled', '');
-  inputContent.labels[0].classList.add('float-label');
-  modalSubmitBtn.textContent = 'Save';
-}
-
-function showAddTodoModal() {
-  modalEl.dataset.mode = 'add-todo';
-  modal.style.display = 'flex';
-  radioFieldset.style.pointerEvents = 'none';
-  inputTodo.checked = true;
-  inputTitle.setAttribute('disabled', '');
-  modalSubmitBtn.textContent = 'Add';
-}
-
-function resetModal() {
-  modalForm.reset();
-  modalEl.dataset.mode = 'create';
-  radioFieldset.style.removeProperty('pointer-events');
-  inputTitle.removeAttribute('disabled');
-  inputContent.labels[0].classList.remove('float-label');
-  modalSubmitBtn.textContent = 'Create';
-}
-
-function floatLabel() {
-  if (this.value && this.value !== ' ') {
-    this.labels[0].classList.add('float-label');
-  } else {
-    this.labels[0].classList.remove('float-label');
-  }
-}
-
-[inputTitle, inputContent].forEach((el) => {
-  el.addEventListener('input', floatLabel);
-});
-
-window.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-});
-
-modalCloseBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-openModalBtn.addEventListener('click', () => {
-  modal.style.display = 'flex';
-  resetModal();
-});
-
-document.querySelector('main').addEventListener('click', (e) => {
-  const btn = e.target;
-
-  if (btn.className === 'todo-btn-edit') {
-    showEditModal();
-  } else if (btn.classList.contains('btn-add-project-todo')) {
-    showAddTodoModal();
-  }
-});
-
-function currentDate() {
-  return new Date().toISOString().split('T')[0];
-}
-
-function setMinDate() {
-  inputDate.setAttribute('min', currentDate());
-}
-setMinDate();
