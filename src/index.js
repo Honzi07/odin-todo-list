e2310bc7b572be0cc95fb4518b025dc093b79de6
Honzi07 <nyisztor.zsolt@gmail.com>
2024-04-28 20:49:52 +0200

    Import module and create class instances.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import './modules/logic.js';
-import './modules/create.js';
-import './modules/project.js';
-import './modules/todo.js';
-import './modules/dom.js';
+import './modules/dom_o.js';
 import './scss/style.scss';
 import './scss/reset.scss';
+import Create from './modules/create.js';
+import Project from './modules/project.js';
+import Todo from './modules/todo.js';
+import DOM from './modules/dom.js';
 
-const menuBtn = document.querySelector('.btn-menu');
-const asideEL = document.querySelector('aside');
-const mainEL = document.querySelector('main');
-const createProjectBtn = document.querySelector('.btn-create-project');
-const modal = document.querySelector('.modal');
-const modalEl = document.querySelector('.modal-content');
-const modalCloseBtn = document.querySelector('.modal-close');
-const projectEl = document.querySelectorAll('.project');
-const projectInputContainer = document.querySelector(
-  '.project-input-container'
-);
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('active');
-  asideEL.classList.toggle('active');
-  mainEL.classList.toggle('active');
-});
+const domCl = new DOM();
+const createCl = new Create();
