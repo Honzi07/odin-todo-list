6cbee680e0701ce39b2c2cedc3a0174918e98a42
Honzi07 <nyisztor.zsolt@gmail.com>
2024-05-09 04:23:28 +0200

    Added method to search array for clicked element and retrieve its data.

--- src/modules/create.js
+++ src/modules/create.js
@@ -28,6 +28,37 @@ export default class Create {
     return Create.tasksArr.filter((obj) => obj.type === type);
   }
 
+  getClickedElement(ev, arr) {
+    const closestTodo = ev.target.closest('.todo');
+    const closestProject = ev.target.closest('.project');
+
+    let todo;
+    let project;
+
+    if (closestTodo && closestProject) {
+      const projectData = arr.find(
+        (el) => el.id === +closestProject.dataset.id
+      );
+      if (projectData) {
+        project = projectData;
+        todo = project.tasks.find((el) => el.id === +closestTodo.dataset.id);
+      }
+    } else if (closestTodo) {
+      todo = arr.find((el) => el.id === +closestTodo.dataset.id);
+    } else if (closestProject) {
+      project = arr.find((el) => el.id === +closestProject.dataset.id);
+    }
+
+    return {
+      todo,
+      project,
+      element: {
+        todoEl: closestTodo,
+        projectEl: closestProject,
+      },
+    };
+  }
+
   insertHtml(parentEl, html) {
     parentEl.insertAdjacentHTML('afterbegin', html);
   }
