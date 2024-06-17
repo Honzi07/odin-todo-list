e1d7a1e17d0077c190b4fe9a8a369b92460dea1f
Honzi07 <nyisztor.zsolt@gmail.com>
2024-06-17 20:17:23 +0200

    Transferred and updated all functionalities from the old DOM file.

--- src/modules/domManager.js
+++ src/modules/domManager.js
@@ -5,19 +5,25 @@ export default class DOMManager {
   }
 
   initMethods() {
-    this.modalEvents();
+    this.modalEventsListeners();
+    this.toggleMobileMenu();
+    this.inputStateEventListener();
+    this.floatLabels();
+    this.toggleAddEditTodoProperties();
   }
 
   resetModal() {
+    const { form, fieldsetRadio, inputTitle, inputContent, btnSubmit } =
+      this.allElements;
     form.reset();
     form.dataset.mode = 'create';
-    radioFieldset.style.removeProperty('pointer-events');
+    fieldsetRadio.style.removeProperty('pointer-events');
     inputTitle.removeAttribute('disabled');
     inputContent.labels[0].classList.remove('float-label');
-    modalSubmitBtn.textContent = 'Create';
+    btnSubmit.textContent = 'Create';
   }
 
-  modalEvents() {
+  modalEventsListeners() {
     const modal = this.allElements.modal;
 
     window.addEventListener('click', (e) => {
@@ -28,11 +34,88 @@ export default class DOMManager {
 
     this.allElements.btnCloseModal.addEventListener('click', () => {
       modal.style.display = 'none';
+      this.resetModal();
     });
 
     this.allElements.btnOpenModal.addEventListener('click', () => {
       modal.style.display = 'flex';
-      // resetModal();
+      this.resetModal();
+    });
+  }
+
+  toggleMobileMenu() {
+    const { btnMobileMenu, asideEl, mainEl } = this.allElements;
+    btnMobileMenu.addEventListener('click', () => {
+      btnMobileMenu.classList.toggle('active');
+      asideEl.classList.toggle('active');
+      mainEl.classList.toggle('active');
+    });
+  }
+
+  inputStateEventListener() {
+    const { inputTodo, inputProject } = this.allElements;
+
+    [inputTodo, inputProject].forEach((el) => {
+      el.addEventListener('click', this.updateInputState.bind(this));
+    });
+  }
+
+  updateInputState() {
+    const { inputTodo, inputTitle, inputContent } = this.allElements;
+
+    if (inputTodo.checked) {
+      inputTitle.removeAttribute('disabled');
+      inputTitle.setAttribute('disabled', '');
+      inputTitle.classList.add('disabled');
+      inputTitle.labels[0].classList.add('disabled');
+      inputContent.setAttribute('required', '');
+    } else {
+      inputTitle.removeAttribute('disabled');
+      inputTitle.setAttribute('required', '');
+      inputTitle.classList.remove('disabled');
+      inputTitle.labels[0].classList.remove('disabled');
+      inputContent.removeAttribute('required');
+      inputContent.setAttribute('required', '');
+    }
+  }
+
+  floatLabels() {
+    const { inputTitle, inputContent } = this.allElements;
+
+    const updateLabel = function () {
+      if (this.value && this.value !== ' ') {
+        this.labels[0].classList.add('float-label');
+      } else {
+        this.labels[0].classList.remove('float-label');
+      }
+    };
+
+    [inputTitle, inputContent].forEach((el) => {
+      el.addEventListener('input', updateLabel);
+    });
+  }
+
+  toggleAddEditTodoProperties() {
+    const el = this.allElements;
+
+    const changeProperties = (mode, btnText) => {
+      el.form.dataset.mode = mode;
+      el.btnSubmit.textContent = btnText;
+      el.modal.style.display = 'flex';
+      el.fieldsetRadio.style.pointerEvents = 'none';
+      el.inputTodo.checked = true;
+      el.inputTitle.setAttribute('disabled', '');
+    };
+
+    el.mainEl.addEventListener('click', (e) => {
+      const btn = e.target;
+      if (btn.dataset.btnType === 'edit') {
+        changeProperties('edit', 'Save');
+        el.inputContent.labels[0].classList.add('float-label');
+        el.inputTitle.labels[0].classList.add('float-label');
+      } else if (btn.dataset.btnType === 'add-todo') {
+        changeProperties('add-todo', 'Add');
+      }
     });
   }
 }
