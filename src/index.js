9ee4fab89de715b804a37168a1b5e9a6824fcc1d
Honzi07 <nyisztor.zsolt@gmail.com>
2024-06-23 21:18:33 +0200

    Converted reset SCSS file to a partial.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './modules/logic.js';
 import './modules/dom_o.js';
 import './scss/style.scss';
-import './scss/reset.scss';
+// import './scss/reset.scss';
 import Create from './modules/create.js';
 import Project from './modules/project.js';
 import Todo from './modules/todo.js';
